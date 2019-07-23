d671c9ad59b1d16aabbf21eb24500bed5257e8c5
dsino <dsino@K1807N0021937>
2019-07-23 12:58:44 +0300

    Форматирование

--- src/models/mort-settings.ts
+++ src/models/mort-settings.ts
@@ -1,11 +1,11 @@
 import { CommonHelper } from '../helpers/сommon-helper';
 
-export class MortSettings{
-    propPrice : number = 7_000_000;   // цена квартиры
-    mortRate : number = 6;            // ставка по ипотеке
-    comFeesM : number = 3500;         // коммунальные платежи без счетчиков (фикс), в мес
-    propTaxY : number = 30_000;       // налог, страховка, прочие платежи владения квартирой, в год
-    comRateY : number = 2;            // удорожание коммуналки, % в год
+export class MortSettings {
+  propPrice: number = 7_000_000;   // цена квартиры
+  mortRate: number = 6;            // ставка по ипотеке
+  comFeesM: number = 3500;         // коммунальные платежи без счетчиков (фикс), в мес
+  propTaxY: number = 30_000;       // налог, страховка, прочие платежи владения квартирой, в год
+  comRateY: number = 2;            // удорожание коммуналки, % в год
 
-    public get comFeesIn10Years() {return CommonHelper.inflateYearly(this.comFeesM, this.comRateY, 120);}
-  }
+  public get comFeesIn10Years() { return CommonHelper.inflateYearly(this.comFeesM, this.comRateY, 120); }
+}
